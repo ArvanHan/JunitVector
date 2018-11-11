74e96144c60919b5cc0d04e0e54ee6516e95cdb5
YansanHan <34778589+YansanHan@users.noreply.github.com>
2018-11-11 17:55:50 -0500

    update 2 add funtion with no implementation and some test

--- src/MyVector.java
+++ src/MyVector.java
@@ -1,7 +1,7 @@
-public class MyVector <E>{
+public final class MyVector {
 
-    protected Object[] data; //Vector itself
-    protected int size; // maximum capacity of the vector
+    protected final Object[] data; //Vector itself
+    protected final int size; // maximum capacity of the vector
 
     public MyVector() {
         this(0, 0);
@@ -25,4 +25,67 @@ public class MyVector <E>{
         for (int i = 0; i < size; i++) data[i] = I[i];
     }
 
+    public MyVector append() {
+        return null;
+    }
+
+    public MyVector append(int[] intArray) {
+        return null;
+    }
+
+    public MyVector append(MyVector V) {
+        return null;
+    }
+
+    public MyVector append(double aDouble) {
+        return null;
+    }
+
+    public MyVector clone() {// returns a clone of this
+        return null;
+    }
+
+    public Boolean equal(MyVector V) {//this and V are the same
+        return null;
+    }
+
+    public int getLength() { //returns number of elements in this
+        return 0;
+    }
+
+    public Double getValue(int i) { //returns the value this[i]
+        return null;
+    }
+
+    public MyVector add(MyVector V) {//add this to V, returning a Vector the same size as this
+        return null;
+    }
+
+    public MyVector add(double aDouble) { //add aDouble to every element of this
+        return null;
+    }
+
+    public MyVector sub(MyVector V) {//sub this – V
+        return null;
+    }
+
+    public MyVector subV(int l, int r) {//will return a sub vector between the indices l and r inclusive
+        return null;
+    }
+
+    public MyVector Mult(MyVector V) { //Multiple every element of this by corresponding element in V
+        return null;
+    }
+
+    public MyVector Mult(double aDouble) {//Multiply every element of this by aDouble
+        return null;
+    }
+
+    public MyVector Normalize() {//returns this as a normalized vector
+        return null;
+    }
+
+    public Double EuclidianDistance(MyVector V) {//returns the Euclidian distance between this and V.
+        return null;
+    }
 }
