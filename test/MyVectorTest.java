fa6561ea089c88dcf5f3b7670a52d917da96ae1b
YansanHan <34778589+YansanHan@users.noreply.github.com>
2018-11-12 23:51:41 -0500

    update test and small fix on constructor

--- test/MyVectorTest.java
+++ test/MyVectorTest.java
@@ -4,31 +4,41 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class MyVectorTest {
+    private final double [] ORIGDARRAY = new double[] {-1.1,0.0,1.0,2.3,3.0 };
+    private final int [] ORIGIARRAY = new int[]{3,2,1,0,-1};
+
     @Test //test get value
     public void getValue(){
-        double [] doubleVector= new double[] {0.0,1.0,2.0,3.0 };
-        MyVector vector = new MyVector(doubleVector);
-        assertEquals(2.0,(double)vector.getValue(3));
+        MyVector vector = new MyVector(ORIGDARRAY);
+        MyVector emptyVector = new MyVector();
+        assertEquals(2.0,(double)vector.getValue(3),"test getValue from double vector");
         assertEquals(3.0,(double)vector.getValue(4));
-        int [] intArray = new int[]{1,3,5,7};
-        MyVector intVector = new MyVector(intArray);
-        assertEquals(5,(int)vector.getValue(3));
+        assertEquals(3.0,(double)vector.getValue(5),"test getValue from index out of bound");
+        assertEquals(null,(double)vector.getValue(3),"test getValue from empty vector");
+        MyVector intVector = new MyVector(ORIGIARRAY);
+        assertEquals(5,(int)vector.getValue(3),"test getValue from int vector");
         assertEquals(7,(int)vector.getValue(4));
-
     }
 
     @Test //test equal
     public void testEqual(){
-        MyVector vector = new MyVector();
-        System.out.println("");
-        assertEquals("",vector);
+        MyVector vD1 = new MyVector(ORIGDARRAY);
+        MyVector vD2 = new MyVector(ORIGDARRAY);
+        double[] doubleArray = new double[]{3.0,2.3,1.0,0.0,-1.1};
+        MyVector diffVD3 = new MyVector(doubleArray);
+        assertEquals(true,vD1.equal(vD2));
+        assertEquals(false,vD1.equal(diffVD3));
+
+        MyVector vI1 = new MyVector(ORIGIARRAY);
+        MyVector vI2 = new MyVector(ORIGIARRAY);
+        assertEquals(true,vI1.equal(vI2));
     }
 
     @Test //test append double[]
     public void testAppendDdouble(){
-        MyVector vector = new MyVector();
+        MyVector vector = new MyVector(ORIGDARRAY);
         System.out.println("");
-        assertEquals("",vector);
+        assertEquals("",vector.append(ORIGDARRAY));
     }
 
     @Test //test append int[]
@@ -48,8 +58,7 @@ class MyVectorTest {
 
     @Test //test getlength
     public void testGetLength(){
-        double [] doubleVector= new double[] {0.0,1.0,2.0,3.0 };
-        MyVector vector = new MyVector(doubleVector);
+        MyVector vector = new MyVector(ORIGDARRAY);
         System.out.println("testGetLength");
         int length = vector.getLength();
         assertEquals("4",length);
@@ -57,8 +66,7 @@ class MyVectorTest {
 
     @Test //test add vector
     public void testAdd(){
-        double [] original = new double[] {0.0,1.0,2.0,3.0 };
-        MyVector vector = new MyVector(original);
+        MyVector vector = new MyVector(ORIGDARRAY);
         System.out.println("");
         assertEquals( "",vector);
     }
@@ -66,10 +74,9 @@ class MyVectorTest {
     @Test //test add double
     public void testAddDouble(){
         double two = 2.0;
-        double [] original = new double[] {0.0,1.0,2.0,3.0 };
-        double [] expect = new double[]{2,3,4,5};
-        MyVector vector = new MyVector(original);
-        MyVector result = vector.add(two);
+        double [] expect = new double[]{0.9,2,3,4.3,5};
+        MyVector vectorD = new MyVector(ORIGDARRAY);
+        MyVector result = vectorD.add(two);
         MyVector expectV = new MyVector(expect);
         assertEquals(expectV,result);
     }
@@ -90,16 +97,17 @@ class MyVectorTest {
 
     @Test //test mult vector
     public void testMult(){
-        MyVector vector = new MyVector();
-        System.out.println("");
-        assertEquals("",vector);
+        MyVector vectorD1 = new MyVector(ORIGDARRAY);
+        MyVector vectorD2 = new MyVector(ORIGDARRAY);
+        Double[] tempDouble = new Double[]{3.0,2.3,1.0,0.0,-1.1};
+        assertEquals("",vectorD1);
     }
 
     @Test //test mult double
     public void testMultDouble(){
-        MyVector vector = new MyVector();
+        MyVector vector = new MyVector(ORIGDARRAY);
         System.out.println("");
-        assertEquals("",vector);
+        assertEquals("",vector.mult(2));
     }
 
     @Test //test normalize
